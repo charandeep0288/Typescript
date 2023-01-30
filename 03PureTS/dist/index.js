ed9f392a95b2b874110ee0d0f9d2d4bb95848d52
Charandeep.Singh <Charandeep.Singh@discounttire.com>
2023-01-30 21:12:56 +0530

    Typescript - How to setup typescipt for real projects

---
+++ 03PureTS/dist/index.js
@@ -0,0 +1,9 @@
+"use strict";
+// In "tsconfig.json", at line 52 it is mentioned the output directory location where JS file would get generated. 
+// all the .ts files run in the project and output would be generated in ./dist/index.js
+console.log("Typescript is here");
+console.log("Typescript is here");
+// tsc --init => to create "tsconfig.json" file
+// npm init -y => to get "package.json" file
+// tsc -w => watch mode for ".ts" file and generate JS file on specified location in tsconfig.json
+// npm i lite-sever => to run project in the browser & define in package.json, then do -> npm start
